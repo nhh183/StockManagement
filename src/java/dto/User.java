2a9a3b981b597d410ab127efdaa508be6c326ffa
NHH <96916637+nhh183@users.noreply.github.com>
2025-06-05 15:36:46 +0700

    Revert "Quan ly user"

--- src/java/dto/User.java
+++ src/java/dto/User.java
@@ -9,48 +9,5 @@ package dto;
  * @author NHH
  */
 public class User {
-    private String userID;
-    private String fullName;
-    private String roleID;
-    private String password;
-
-    public User(String userID, String fullName, String roleID, String password) {
-        this.userID = userID;
-        this.fullName = fullName;
-        this.roleID = roleID;
-        this.password = password;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getRoleID() {
-        return roleID;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setRoleID(String roleID) {
-        this.roleID = roleID;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
     
 }
